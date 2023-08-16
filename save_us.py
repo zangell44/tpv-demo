3d7310022a8398db92d1b0cf71ff4ca8db749d8c
Zach Angell <42625717+zangell44@users.noreply.github.com>
2023-08-16 11:07:52 -0400

    Update save_us.py

--- save_us.py
+++ save_us.py
@@ -1,6 +1,8 @@
 from prefect import flow
 from prefect.artifacts import create_markdown_artifact
 
+import time
+
 
 @flow
 def save_us():
@@ -8,6 +10,9 @@ def save_us():
     A flow that saves the United States from a zombie apocalypse by
     uploading an artifact to Prefect Cloud.
     """
+    # Sleep for a bunch of time to simulate a long-running flow
+    for _ in range(100):
+        time.sleep(30)
     create_markdown_artifact(
         markdown="Antidote",
         key="antidote"
