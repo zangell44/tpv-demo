01724aaed595a253bd1a866a3d45ef53976f0cc8
zangell44 <zachary.james.angell@gmail.com>
2023-12-16 08:46:48 -0500

    Use run input

--- live_debugger.py
+++ live_debugger.py
@@ -1,5 +1,8 @@
+from pydantic import Field
+
 from prefect import flow, get_run_logger
 from prefect.engine import pause_flow_run
+from prefect.input import RunInput
 
 @flow
 async def input_flow():        
@@ -7,15 +10,18 @@ async def input_flow():
     x = 1
     y = 2
 
+    class DebuggerCommand(RunInput):
+        command: str = Field(..., description="The debug to execute")
+
     logger = get_run_logger()
 
     while True:
-        input_str = await pause_flow_run(wait_for_input=str, timeout=99999)
-        logger.info(f">>> {input_str}")
-        if input_str == "q":
+        input_str = await pause_flow_run(wait_for_input=DebuggerCommand, timeout=99999)
+        logger.info(f">>> {input_str.command}")
+        if input_str.command == "q":
             break
         else:
-            result = eval(input_str, globals(), locals())
+            result = eval(input_str.command, globals(), locals())
             logger.info(result)
 
 
